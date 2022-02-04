68f218757297b8d9abb2c0100aced264ee5bb211
dlans <wy2857749@163.com>
2022-02-04 22:28:58 +0800

    è蓝牙开启完成，设计界面中

--- app/src/main/java/com/example/bluetooth/MainActivity.java
+++ app/src/main/java/com/example/bluetooth/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.bluetooth;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.bluetooth.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BluetoothAdapter bluetoothAdapter;
+    private ActivityMainBinding binding;
+    private final int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_device, R.id.navigation_data).build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        // 获取蓝牙适配器
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+
+        // 检查蓝牙是否打开，如果没有则申请打开
+        // 如果拒绝打开，则退出应用。处理逻辑在onActivityResult
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+            Toast.makeText(this, "蓝牙未打开", Toast.LENGTH_SHORT).show();
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "蓝牙已打开", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "打开失败", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 }
