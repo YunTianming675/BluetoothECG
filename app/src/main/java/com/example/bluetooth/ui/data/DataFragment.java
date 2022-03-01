4bf7b866f7620e01dabf1558f02f9608f3ecf33c
dlans <wy2857749@163.com>
2022-03-01 21:59:34 +0800

    能控制模块了，考虑如何读取数据

--- app/src/main/java/com/example/bluetooth/ui/data/DataFragment.java
+++ app/src/main/java/com/example/bluetooth/ui/data/DataFragment.java
@@ -5,19 +5,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.bluetooth.LogUtil;
 import com.example.bluetooth.adapters.BtAdapter;
 import com.example.bluetooth.databinding.FragmentDataBinding;
 import com.jjoe64.graphview.GraphView;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
 
 public class DataFragment extends Fragment {
 
@@ -27,6 +28,7 @@ public class DataFragment extends Fragment {
     private FragmentDataBinding binding;
     private GraphView graphView;
     private AppCompatButton buttonStart;
+    private AppCompatButton buttonEnd;
     private BluetoothSocket bluetoothSocket;
 
     @Nullable
@@ -35,19 +37,7 @@ public class DataFragment extends Fragment {
         dataViewModel = new ViewModelProvider(this).get(DataViewModel.class);
         binding = FragmentDataBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
         init();
-
-//        final TextView textView = binding.fragDataText1;
-//        graphView = binding.fragDataGraphview;
-//        buttonStart = binding.fragButtonStart;
-
-//        dataViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(String s) {
-//                textView.setText(s);
-//            }
-//        });
         return root;
     }
 
@@ -58,12 +48,49 @@ public class DataFragment extends Fragment {
     }
 
     private void init() {
+        boolean isRead = false;
+
         graphView = binding.fragDataGraphview;
         buttonStart = binding.fragButtonStart;
+        buttonEnd = binding.fragButtonEnd;
 
         buttonStart.setOnClickListener(view -> {
             LogUtil.d(TAG, "on frag_button_start Listener");
+            buttonStart.setEnabled(false);
             bluetoothSocket = BtAdapter.getBtAdapter().getBluetoothSocket();
+
+            try {
+                DataOutputStream outputStream = new DataOutputStream(bluetoothSocket.getOutputStream());
+                outputStream.writeByte(0x8A);
+                LogUtil.d(TAG, "has been sent 0x8A");
+            }
+            catch (IOException e) {
+                LogUtil.e(TAG, "IOException:");
+                e.printStackTrace();
+            }
+
+            // 在子线程中接收数据
+            Thread thread = new Thread(() -> {
+                LogUtil.d(TAG, "on Thread");
+//                while (isRead) {
+//                }
+            });
+            thread.start();
+        });
+
+        buttonEnd.setOnClickListener(view -> {
+            LogUtil.d(TAG, "on frag_button_end Listener");
+            buttonStart.setEnabled(true);
+
+            try {
+                DataOutputStream outputStream = new DataOutputStream(bluetoothSocket.getOutputStream());
+                outputStream.writeByte(0x88);
+                LogUtil.d(TAG, "has been sent 0x88");
+            }
+            catch (IOException e) {
+                LogUtil.e(TAG, "IOException");
+                e.printStackTrace();
+            }
         });
     }
 }
